d6f9cb8a5d1fd881618022eb5020f0d53a597640
azfan1273-jpg <azfan1273@gmail.com>
2026-08-07 02:22:08 +0700

    31

--- js/supabase.js
+++ js/supabase.js
@@ -1,16 +1,12 @@
 // ==========================================
 // FILE: js/supabase.js
 // ==========================================
-
 // Ganti URL dan ANON KEY di bawah ini dengan milikmu yang asli!
 const SUPABASE_URL = "https://qcpxitlltkkxtdctertz.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_CeC_XB7zVoD26xBHmsKHKw_VxqWhPAG";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjcHhpdGxsdGtreHRkY3RlcnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjYwODYsImV4cCI6MjEwMTM0MjA4Nn0.RCZhreRKQIkxgsJXsfAZZia-AEgd_vM3DtgjkFXCYcI";
 
-// Deklarasikan variabel secara GLOBAL menggunakan 'var' atau 'window'
+// Deklarasikan variabel secara GLOBAL
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Fallback untuk kompabilitas dengan kode yang memanggil 'supabase' saja
 window.supabase = window.supabaseClient;
 
-// Cek koneksi awal
 console.log('✅ Supabase Client Initialized:', window.supabaseClient);
